503ca51aae84b44f7d56e68bf4dfd927965a7f9f
Сергей Горячев <serezha_goryachev@list.ru>
2023-07-24 08:44:17 +0400

    Написал функцию рисования пузырька c двумя параметрами

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -5,15 +5,22 @@ import simple_draw as sd
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-point = sd.get_point(500, 300)
-radius = 65
-
-for _ in range(3):
-    radius += 5
-    sd.circle(center_position=point, radius=radius)
+# point = sd.get_point(500, 300)
+# radius = 65
+#
+# for _ in range(3):
+#     radius += 5
+#     sd.circle(center_position=point, radius=radius)
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+def draw_bubble(point, step):
+    radius = 65
+    for _ in range(3):
+        radius += step
+        sd.circle(center_position=point, radius=radius)
+
+point = sd.get_point(600, 400)
+draw_bubble(point, 15)
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
