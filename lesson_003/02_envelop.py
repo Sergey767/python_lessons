c6db8b654d82d5a750dba9271d0fa89903931af5
Сергей Горячев <serezha_goryachev@list.ru>
2023-07-25 21:13:32 +0400

    Написал проверку поместится ли бумага в конверте и пройдет ли кирпич через отверстие с использованием операторов if/elif/else

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -20,7 +20,12 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if envelop_x >= paper_x and envelop_y >= paper_y:
+    print('Да')
+elif envelop_x >= paper_y and envelop_y >= paper_x:
+    print('Да')
+else:
+    print('Нет')
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,25 +33,33 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
 # brick_x, brick_y, brick_z = 2, 10, 11
 # brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
-# brick_x, brick_y, brick_z = 3, 6, 5
-# brick_x, brick_y, brick_z = 6, 3, 5
-# brick_x, brick_y, brick_z = 6, 5, 3
-# brick_x, brick_y, brick_z = 5, 6, 3
-# brick_x, brick_y, brick_z = 5, 3, 6
-# brick_x, brick_y, brick_z = 11, 3, 6
-# brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
-# brick_x, brick_y, brick_z = 6, 3, 11
-# brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
+# brick_x, brick_y, brick_z = 3, 5, 6   #да
+# brick_x, brick_y, brick_z = 3, 6, 5 #да
+# brick_x, brick_y, brick_z = 6, 3, 5 #да
+# brick_x, brick_y, brick_z = 6, 5, 3 #да
+# brick_x, brick_y, brick_z = 5, 6, 3 #да
+# brick_x, brick_y, brick_z = 5, 3, 6 # да
+# brick_x, brick_y, brick_z = 11, 3, 6 #да
+# brick_x, brick_y, brick_z = 11, 6, 3 #да
+# brick_x, brick_y, brick_z = 6, 11, 3 #да
+# brick_x, brick_y, brick_z = 6, 3, 11 #да
+# brick_x, brick_y, brick_z = 3, 6, 11 #да
+# brick_x, brick_y, brick_z = 3, 11, 6 #да
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if brick_x <= hole_x >= brick_y and brick_x <= hole_y >= brick_y:
+    print('Да')
+elif brick_y <= hole_x >= brick_z and brick_y <= hole_y >= brick_z:
+    print('Да')
+elif brick_x <= hole_x >= brick_z and brick_x <= hole_y >= brick_z:
+    print('Да')
+else:
+    print('Нет')
+
