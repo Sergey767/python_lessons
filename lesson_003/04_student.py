cd4df3ecca9cb7ad748a13997d171484992c6a19
Сергей Горячев <serezha_goryachev@list.ru>
2023-07-26 23:32:07 +0400

    Составил программу расчета суммы денег

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -11,4 +11,21 @@
 
 educational_grant, expenses = 10000, 12000
 
-# TODO здесь ваш код
+percent = 3 / 100
+academic_year = 10
+number_month = 0
+percent_expenses = 0
+high_expenses = expenses
+
+while number_month < academic_year:
+    number_month += 1
+    if number_month == 1:
+        continue
+    percent_expenses = (high_expenses * percent) + expenses
+    high_expenses += percent_expenses
+amount_money = high_expenses - (educational_grant * 10)
+print(f'Студенту надо попросить {amount_money} рублей')
+
+
+
+
