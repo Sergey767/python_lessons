82fb0250a64bc09fcd9b07ed6968a141aabf0aed
Сергей Горячев <serezha_goryachev@list.ru>
2023-08-06 19:03:35 +0400

    Написал функции рисования равносторонних геометрических фигур

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -27,7 +27,75 @@ import simple_draw as sd
 # sd.line()
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
-# TODO здесь ваш код
+point_triangle = sd.get_point(100, 100)
+length = 100
+angle = 0
+
+def draw_triangle(point, angle, length):
+    vector_1 = sd.get_vector(start_point=point, angle=angle + 20, length=length)
+    vector_1.draw()
+
+    vector_2 = sd.get_vector(start_point=vector_1.end_point, angle=angle + 140, length=length)
+    vector_2.draw()
+
+    vector_3 = sd.get_vector(start_point=vector_2.end_point, angle=angle + 260, length=length)
+    vector_3.draw()
+
+point_square = sd.get_point(400, 100)
+def draw_square(point, angle, length):
+    vector_1 = sd.get_vector(start_point=point, angle=angle + 20, length=length)
+    vector_1.draw()
+
+    vector_2 = sd.get_vector(start_point=vector_1.end_point, angle=angle + 110, length=length)
+    vector_2.draw()
+
+    vector_3 = sd.get_vector(start_point=vector_2.end_point, angle=angle + 200, length=length)
+    vector_3.draw()
+
+    vector_4 = sd.get_vector(start_point=vector_3.end_point, angle=angle + 290, length=length)
+    vector_4.draw()
+
+point_pentagon = sd.get_point(100, 350)
+def draw_pentagon(point, angle, length):
+    vector_1 = sd.get_vector(start_point=point, angle=angle + 30, length=length)
+    vector_1.draw()
+
+    vector_2 = sd.get_vector(start_point=vector_1.end_point, angle=angle + 102, length=length)
+    vector_2.draw()
+
+    vector_3 = sd.get_vector(start_point=vector_2.end_point, angle=angle + 174, length=length)
+    vector_3.draw()
+
+    vector_4 = sd.get_vector(start_point=vector_3.end_point, angle=angle + 246, length=length)
+    vector_4.draw()
+
+    vector_5 = sd.get_vector(start_point=vector_4.end_point, angle=angle + 320, length=length)
+    vector_5.draw()
+
+point_hexagon = sd.get_point(400, 350)
+def draw_hexagon(point, angle, length):
+    vector_1 = sd.get_vector(start_point=point, angle=angle + 20, length=length)
+    vector_1.draw()
+
+    vector_2 = sd.get_vector(start_point=vector_1.end_point, angle=angle + 80, length=length)
+    vector_2.draw()
+
+    vector_3 = sd.get_vector(start_point=vector_2.end_point, angle=angle + 140, length=length)
+    vector_3.draw()
+
+    vector_4 = sd.get_vector(start_point=vector_3.end_point, angle=angle + 200, length=length)
+    vector_4.draw()
+
+    vector_5 = sd.get_vector(start_point=vector_4.end_point, angle=angle + 260, length=length)
+    vector_5.draw()
+
+    vector_6 = sd.get_vector(start_point=vector_5.end_point, angle=angle + 322, length=length)
+    vector_6.draw()
+
+draw_triangle(point=point_triangle, angle=angle, length=length)
+draw_square(point=point_square, angle=angle, length=length)
+draw_pentagon(point=point_pentagon, angle=angle, length=length)
+draw_hexagon(point=point_hexagon, angle=angle, length=length)
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
@@ -51,6 +119,5 @@ import simple_draw as sd
 # А теперь - сколько надо работы что бы внести изменения в код? Выгода на лицо :)
 # Поэтому среди программистов есть принцип D.R.Y. https://clck.ru/GEsA9
 # Будьте ленивыми, не используйте копи-пасту!
-
 
 sd.pause()
