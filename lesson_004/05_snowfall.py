b2bd04628d94ed839d555551f11b39d78396d7fd
Сергей Горячев <serezha_goryachev@list.ru>
2023-08-13 15:46:22 +0400

    Реализовал снегопад: Создал списки данных для отрисовки N снежинок. Нарисовал падение этих N снежинок. Создал список рандомных длинн лучей снежинок. Сделал рандомные отклонения вправо/влево при каждом шаге и сделал сугоб внизу экрана

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -2,13 +2,13 @@
 
 import simple_draw as sd
 
-
+sd.resolution = (1200, 800)
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
+n = 20
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,13 +17,34 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
+x_list = []
+y_list = []
+length_list = []
+
+def add_lists():
+    y_points = sd.random_number(a=500, b=600)
+    x_points = sd.random_number(a=100, b=1000)
+    x_list.append(x_points)
+    y_list.append(y_points)
+    lengths = sd.random_number(a=10, b=100)
+    length_list.append(lengths)
+
 while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
+    for i in range(n):
+        add_lists()
+        sd.start_drawing()
+        snowflake_length = length_list[i]
+        point = sd.get_point(x=x_list[i], y=y_list[i])
+        sd.snowflake(center=point, length=snowflake_length, color=sd.background_color)
+        y_list[i] -= 10
+        x_list[i] += sd.random_number(-10, 10)
+        next_point = sd.get_point(x=x_list[i], y=y_list[i])
+        sd.snowflake(center=next_point, length=snowflake_length, color=sd.COLOR_WHITE)
+        if y_list[i] < 100:
+            y_list[i] = 600
+            continue
+        sd.finish_drawing()
+        sd.sleep(0.01)
     if sd.user_want_exit():
         break
 
