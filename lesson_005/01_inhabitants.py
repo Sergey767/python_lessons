77bfa3a4933089588cd32af7a5150343ded0b0e0
Сергей Горячев <serezha_goryachev@list.ru>
2023-08-14 14:05:24 +0400

    Сделал вывод на консоль жителей комнат (модули room_1 и room_2)

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
-
+import room_1
+from room_2 import folks
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+folks1 = ''
+folks2 = ''
+
+for name in room_1.folks:
+    folks1 += name + ', '
+print(f'В комнате room_1 живут:', folks1)
+
+for name in folks:
+    folks2 += name + ', '
+print(f'В комнате room_2 живут:', folks2)
 
 
