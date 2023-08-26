71a756168f514c4ba4473c984a4f5613b71c270f
Сергей Горячев <serezha_goryachev@list.ru>
2023-08-26 11:42:24 +0400

    Вторая версия игры НИМ.

--- lesson_006/python_snippets/04_practice.py
+++ lesson_006/python_snippets/04_practice.py
@@ -34,9 +34,12 @@ while True:
     cprint('Ход игрока {}'.format(user_number), color=user_color)
     pos = input(colored('Откуда берем? ', color=user_color))
     qua = input(colored('Сколько берём? ', color=user_color))
-    take_from_bunch(position=int(pos), quantity=int(qua))
+    step_successed = take_from_bunch(position=int(pos), quantity=int(qua))
     if is_gameover():
         break
-    user_number = 2 if user_number == 1 else 1
+    if step_successed:
+        user_number = 2 if user_number == 1 else 1
+    else:
+        cprint('Невозможный ход!', color='white')
 
 cprint('Выиграл игрок номер {}'.format(user_number), color='magenta')
